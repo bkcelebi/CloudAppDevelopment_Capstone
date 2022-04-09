87154ac526d1cc65eaf150715571498b7881d60d
Burak <98746081+bkcelebi@users.noreply.github.com>
2022-04-09 14:28:46 +0100

    adjustment on allowed hosts

--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 APPEND_SLASH = True
 
-ALLOWED_HOSTS = ["bkcelebi123.eu-gb.mybluemix.net", "localhost"]
+ALLOWED_HOSTS = ["bkcelebi123.eu-gb.mybluemix.net", "localhost", "istdub2016.eu-gb.mybluemix.net"]
 
 
 # Application definition
